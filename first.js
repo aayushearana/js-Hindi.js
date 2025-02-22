1ef86afacea8679041544e54048cc44c99e46b38
aayushearana <aayushea.rana@gmail.com>
2025-02-22 05:54:40 +0000

    math concept intro

--- first.js
+++ first.js
@@ -1,1 +1,23 @@
-console.log("ayushea");
+//maths
+// console.log(Math);
+// console.log(Math.abs(-5));console.log();
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,5,3,6));
+// console.log(Math.max(5,3,5,6));
+
+// DATE
+let myDate=new Date()
+// 
+let myCreatedDate=new Date("2025-03-02");
+// 
+
+let myTimeStamp=Date.now();
+//
+
+
+
+
+
+
